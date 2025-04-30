159d0264195208fcbee4016392a2093896dc01fd
JonathanFleischmann <i22006@hb.dhbw-stuttgart.degit config --global user.name>
2025-04-30 19:07:05 +0200

    add first version of ui

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from phone_info_extractor import PhoneInfoExtractor
 from phone_number import PhoneNumber
 from number_information import NumberInformation
 
+from user_interface import UserInterface
+
 test_strings = [
     "+49 0201 123456",
     "+44 0201123456",
@@ -23,3 +25,5 @@ for test_string in test_strings:
         print(f"Phone number: {phone_info[0].country_code}, {phone_info[0].area_code}, {phone_info[0].phone_number}, {phone_info[0].extension}")
     except ValueError as e:
         print(f"Error: {e}")
+    
+UserInterface().start_user_interface()
